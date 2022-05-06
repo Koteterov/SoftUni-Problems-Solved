a80a33855532a56f9619649f213bdc794311a947
Koteterov <koteterov@yahoo.com>
2022-05-06 21:28:48 +0300

    [adv func]

---
+++ advanced/advanced-functions/lab/02.Add.js
@@ -0,0 +1,37 @@
+function solution(num) {
+  function add(a, b) {
+    return a + b;
+  }
+  return add.bind(this, num);
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+
+//============================
+// function solution(num) {
+//   function add(a, b) {
+//     return a + b;
+//   }
+//   return (n) => add(n, num);
+// }
+
+// let add5 = solution(5);
+// console.log(add5(2));
+// console.log(add5(3));
+
+
+//=============================
+// function solution(num) {
+//   let number = num;
+
+//  return function add(n) {
+//     return number + n;
+//   }
+// }
+
+// let add5 = solution(5);
+// console.log(add5(2));
+// console.log(add5(3));
