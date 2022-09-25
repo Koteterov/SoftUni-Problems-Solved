590f4db00b49017e606df69afb3e2d7d3e57051c
Dimitar Koteterov <koteterov@yahoo.com>
2022-09-25 21:08:13 +0300

    useless code removed

--- js-back-end/02.SharedTrip/src/controllers/homeController.js
+++ js-back-end/02.SharedTrip/src/controllers/homeController.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
-// const pictureService = require("../services/pictureService");
 
 router.get("/", async (req, res) => {
-  // res.locals.title = "Home"
-
-  // const picturesData = await pictureService.getAll().lean()
-
-  
-  // const pictures = picturesData.map(x => ({...x, shareCount: x.usersShared.length}));
 
   res.render("home")
   
